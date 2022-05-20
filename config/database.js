97bf1c38d125b9de653728191a52c74b3463cbbc
baekhyeyoon <pd206@naver.com>
2022-05-21 03:18:49 +0900

    add database

--- config/database.js
+++ config/database.js
@@ -3,10 +3,10 @@ const {logger} = require('./winston');
 
 // TODO: 본인의 DB 계정 입력
 const pool = mysql.createPool({
-    host: '',
-    user: '',
+    host: 'cmchackathondb.cxcw5prmxvi7.ap-northeast-2.rds.amazonaws.com',
+    user: 'admin',
     port: '3306',
-    password: '',
+    password: 'hannah100',
     database: ''
 });
 
